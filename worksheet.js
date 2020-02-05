e8c6a542515a6275fe7fc3960f3771cbb99e668b
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-05 18:03:50 -0500

    Exercises for Arrays Complete

--- worksheet.js
+++ worksheet.js
@@ -1,3 +1,31 @@
 var colors = ['red', 'orange', 'yellow'];
 
+colors[3] = 'green';
+
 var dogs = ['Rusty', 'Wyatt', 'Olly'];
+
+var friends = ['Charlie', 'Liz', 'David', 'Mattias', 'Liz'];
+
+var fruits = ['banana', 'orange', 'lemon', 'apple', 'mango'];
+
+var nums = [1, 2, 3, 'a', 'b', 445, 34];
+
+// EXERCISE 1
+
+var numbers = [22, 67, 33, 96, 88];
+
+// What does the following line print?
+console.log('EXERCISE 1');
+console.log(numbers[numbers.length]);
+
+// EXERCISE 2
+
+var friendGroups = [
+  ['Harry', 'Ron', 'Hermione'],
+  ['Malfoy', 'Crabbe', 'Goyle'],
+  ['Mooney', 'Wormtail', 'Prongs']
+];
+
+// What is the result of this line:
+console.log('EXERCISE 2');
+console.log(friendGroups[2][0]);
